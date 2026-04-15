a2a4821eeb2a3fd6c7e28068e7076a3e2a382dcb
Kadohov <g.kadokhov@gmail.com>
2026-04-15 10:04:29 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Place}/{action=Index}/{id?}");
 
 app.Run();
